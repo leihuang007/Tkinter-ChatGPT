53ff5c3b8173c57f995c01b109d24cfb0a4bc22d
Easton Huang <leihuang007@qq.com>
2023-02-15 11:34:25 +0800

    Create Submit Button

--- chat.py
+++ chat.py
@@ -15,6 +15,18 @@ customtkinter.set_appearance_mode("dark")
 customtkinter.set_default_color_theme("dark-blue")
 
 
+# Submit to ChatGPT
+def speak():
+    pass
+
+# Clear The Screens
+def clear():
+    pass
+
+# Do API stuff
+def key():
+    pass
+
 # Create Text Frame
 text_frame = customtkinter.CTkFrame(root)
 text_frame.pack(pady=20)
@@ -42,5 +54,23 @@ my_text.configure(yscrollcommand=text_scroll.set)
 chat_entry = customtkinter.CTkEntry(root, placeholder_text="Type Something To ChatGPT...",
                                     width=535, height=50, border_width=1)
 chat_entry.pack(pady=10)
+
+# Create Buttom Frame
+button_frame = customtkinter.CTkFrame(root, fg_color="#242424")
+button_frame.pack(pady=10)
+
+# Create Submit Button
+submit_button = customtkinter.CTkButton(button_frame, text="Speak To ChatGPT", command=speak)
+submit_button.grid(row=0, column=0, padx=25)
+
+# Create Submit Button
+clear_button = customtkinter.CTkButton(button_frame, text="Clear Response", command=clear)
+clear_button.grid(row=0, column=1, padx=35)
+
+# Create Submit Button
+api_button = customtkinter.CTkButton(button_frame, text="Update API Key", command=key)
+api_button.grid(row=0, column=2, padx=25)
+
+
 
 root.mainloop()
